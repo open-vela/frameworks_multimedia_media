1eee4711c0b816d8c1cb7507f041d59cb20df113
jihandong <jihandong@xiaomi.com>
2023-03-08 21:22:40 +0800

    framework/media: change name "*_control()" => "*_handler()"
    Signed-off-by: jihandong <jihandong@xiaomi.com>

--- media_stub.c
+++ media_stub.c
@@ -58,7 +58,7 @@ void media_stub_onreceive(void* cookie, media_parcel* in, media_parcel* out)
     case MEDIA_POLICY_CONTROL:
         media_parcel_read_scanf(in, "%s%s%s%i%i", &param_s1, &param_s2,
             &param_s3, &param_i1, &param_i2);
-        ret = media_policy_control(media_get_policy(), param_s1, param_s2,
+        ret = media_policy_handler(media_get_policy(), param_s1, param_s2,
             param_s3, param_i1, &response, param_i2);
         media_parcel_append_printf(out, "%i%s", ret, response);
         break;
@@ -68,7 +68,7 @@ void media_stub_onreceive(void* cookie, media_parcel* in, media_parcel* out)
     case MEDIA_GRAPH_CONTROL:
         media_parcel_read_scanf(in, "%s%s%s%i", &param_s1, &param_s2,
             &param_s3, &param_i1);
-        ret = media_graph_control(media_get_graph(), param_s1, param_s2,
+        ret = media_graph_handler(media_get_graph(), param_s1, param_s2,
             param_s3, &response, param_i1);
         media_parcel_append_printf(out, "%i%s", ret, response);
         break;
@@ -77,7 +77,7 @@ void media_stub_onreceive(void* cookie, media_parcel* in, media_parcel* out)
         media_parcel_read_scanf(in, "%l%s%s%s%i", &param_u, &param_s1,
             &param_s2, &param_s3, &param_i1);
         handle = param_u ? (void*)(uintptr_t)param_u : cookie;
-        ret = media_player_control(handle, param_s1, param_s2, param_s3,
+        ret = media_player_handler(handle, param_s1, param_s2, param_s3,
             &response, param_i1);
         media_parcel_append_printf(out, "%i%s", ret, response);
         break;
@@ -86,7 +86,7 @@ void media_stub_onreceive(void* cookie, media_parcel* in, media_parcel* out)
         media_parcel_read_scanf(in, "%l%s%s%s%i", &param_u, &param_s1,
             &param_s2, &param_s3, &param_i1);
         handle = param_u ? (void*)(uintptr_t)param_u : cookie;
-        ret = media_recorder_control(handle, param_s1, param_s2, param_s3,
+        ret = media_recorder_handler(handle, param_s1, param_s2, param_s3,
             &response, param_i1);
         media_parcel_append_printf(out, "%i%s", ret, response);
         break;
