dffacfb7c993122f494b0fc7bd6d97aae4f51029
qiaohaijiao1 <qiaohaijiao1@xiaomi.com>
2024-05-10 09:37:19 +0800

    framework/media: add AUX_DIGITAL device.
    AUX_DIGITAL used to Android-Vela I2S device
    
    Signed-off-by: qiaohaijiao1 <qiaohaijiao1@xiaomi.com>

--- include/media_defs.h
+++ include/media_defs.h
@@ -162,6 +162,9 @@ typedef void (*media_focus_callback)(int suggestion, void* cookie);
 #define MEDIA_DEVICE_MIC "mic"
 #define MEDIA_DEVICE_MODEM "modem"
 
+#define MEDIA_DEVICE_IN_AUX_DIGITAL "digital_in"
+#define MEDIA_DEVICE_OUT_AUX_DIGITAL "digital_out"
+
 /**
  * @brief Callback to receive current criterion value.
  *
