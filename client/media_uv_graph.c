b2032404868f9ba086aab5ab2dfe1ad2ce86ced5
niyinghao <niyinghao@xiaomi.com>
2024-11-15 19:08:13 +0800

    Fix the issue of illegal player/recorder parameters causing close crash

--- client/media_uv_graph.c
+++ client/media_uv_graph.c
@@ -740,12 +740,13 @@ int media_uv_player_close(void* handle, int pending, media_uv_callback on_close)
     snprintf(tmp, sizeof(tmp), "%d", pending);
     ret = media_uv_stream_send(priv, NULL, "close", tmp, 0,
         media_uv_stream_receive_cb, media_uv_stream_close_cb, priv);
-    if (ret < 0)
-        media_uv_stream_close_cb(priv, ret);
 
     media_uv_stream_close_pipe(handle);
     media_uv_stream_listen_clear(handle, NULL);
     media_uv_stream_abandon_focus(handle);
+
+    if (ret < 0)
+        media_uv_stream_close_cb(priv, ret);
 
     return ret;
 }
@@ -1105,12 +1106,13 @@ int media_uv_recorder_close(void* handle, media_uv_callback on_close)
     priv->on_close = on_close;
     ret = media_uv_stream_send(priv, NULL, "close", "0", 0,
         media_uv_stream_receive_cb, media_uv_stream_close_cb, priv);
-    if (ret < 0)
-        media_uv_stream_close_cb(priv, ret);
 
     media_uv_stream_close_pipe(handle);
     media_uv_stream_listen_clear(handle, NULL);
     media_uv_stream_abandon_focus(handle);
+
+    if (ret < 0)
+        media_uv_stream_close_cb(priv, ret);
 
     return ret;
 }
