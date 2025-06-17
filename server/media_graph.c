831ef9b231e006622762410a491997962359b223
jinxiuxu <jinxiuxu@xiaomi.com>
2025-06-17 11:44:18 +0800

    Revert "media: map cmd process before format_transfer"
    This reverts commit 6254ccbbc338a669d36197f6107167a50be170fc.
    
    Signed-off-by: jinxiuxu <jinxiuxu@xiaomi.com>

--- server/media_graph.c
+++ server/media_graph.c
@@ -548,9 +548,6 @@ static int media_graph_dequeue_command(MediaGraphPriv* priv, bool process)
     av_log(NULL, AV_LOG_INFO, "process %s %s %s\n",
         cmd->filter->name, cmd->cmd, cmd->arg ? cmd->arg : "_");
 
-    ret = avfilter_process_command(cmd->filter, cmd->cmd, cmd->arg,
-        cmd->res, 0, 0);
-
     if (!strcmp(cmd->cmd, "link") || !strcmp(cmd->cmd, "map")) {
         for (i = 0; i < cmd->filter->nb_outputs; i++) {
             FilterLinkInternal* li = (FilterLinkInternal*)cmd->filter->outputs[i];
@@ -566,6 +563,9 @@ static int media_graph_dequeue_command(MediaGraphPriv* priv, bool process)
         if (ret < 0)
             MEDIA_ERR("media graph link error ret:%d:%s\n", ret, av_err2str(ret));
     }
+
+    ret = avfilter_process_command(cmd->filter, cmd->cmd, cmd->arg,
+        cmd->res, 0, 0);
 
     pthread_mutex_lock(&priv->qlock);
     TAILQ_REMOVE(&priv->cmdq, cmd, entries);
