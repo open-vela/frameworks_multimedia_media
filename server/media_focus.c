bdcecdc68215563e06919f156ac1de71f284c0ba
zhangsenlin <zhangsenlin@xiaomi.com>
2025-07-14 15:47:39 +0800

    media/focus:Add the event of media focus delay acknowledgment.
    If "abandon" is not processed immediately, no ack will be sent to the client. As a result, the client cannot receive the ack and thus cannot proceed with subsequent processes.
    
    Signed-off-by: zhangsenlin <zhangsenlin@xiaomi.com>

--- server/media_focus.c
+++ server/media_focus.c
@@ -645,14 +645,10 @@ static int media_focus_handler_l(media_focus* focus, void* cookie, const char* n
         return MEDIA_ERROR_DELAY_ACK;
     } else if (!strcmp(cmd, "abandon")) {
         focus_handle = media_server_get_data(cookie);
-        media_stub_notify_finalize(&cookie);
         ret = media_focus_abandon_(priv, focus_handle);
-        media_focus_queue_pop(priv);
-        if (!SIMPLEQ_EMPTY(&focus->req_q)) {
-            media_focus_request_t* req = (media_focus_request_t*)SIMPLEQ_FIRST(&focus->req_q);
-            media_focus_handler_l(req->focus, req->cookie, req->name, req->cmd, req->req_id, NULL, 0);
-        }
-        return ret;
+        media_focus_send_suggestion(focus, ret, cookie);
+        media_stub_notify_finalize(&cookie);
+        return MEDIA_ERROR_DELAY_ACK;
     } else if (!strcmp(cmd, "dump")) {
         media_focus_debug_stack_display();
         return 0;
