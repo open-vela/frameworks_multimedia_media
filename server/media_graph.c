af8cd1bb84a0dde277a0627202b993b75b7aa8ca
chenyu29 <chenyu29@xiaomi.com>
2025-07-29 15:48:18 +0800

    media: increase AVFILTER_FLAG_SUPPORT_POLL to fix out of bounds
    Signed-off-by: chenyu29 <chenyu29@xiaomi.com>

--- server/media_graph.c
+++ server/media_graph.c
@@ -49,7 +49,7 @@
  ****************************************************************************/
 
 #define MAX_GRAPH_SIZE 4096
-#define MAX_POLL_FILTERS 24
+#define MAX_POLL_FILTERS 32
 
 #define MediaPlayerPriv MediaFilterPriv
 #define MediaRecorderPriv MediaFilterPriv
