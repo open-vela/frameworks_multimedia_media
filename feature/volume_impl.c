97ed1dd60907ca3c61984bc1c79773e04032d51c
chenyu29 <chenyu29@xiaomi.com>
2025-09-16 11:27:06 +0800

    media: add volume limit and notify
    Signed-off-by: chenyu29 <chenyu29@xiaomi.com>

--- feature/volume_impl.c
+++ feature/volume_impl.c
@@ -21,6 +21,7 @@
 
 static const char* file_tag = "[jidl_feature] volume_impl";
 #define ERROR_CODE 202
+#define ERROR_BCL 209
 
 enum OP {
     VOLUME_OP_GET,
@@ -37,37 +38,30 @@ typedef struct {
     double value;
 } VolumeHandle;
 
-void system_volume_onRegister(const char* feature_name)
+typedef struct {
+    void* policy_handle;
+    FeatureInstanceHandle feature;
+} SubscriberHandle;
+
+static void volume_change_callback(void* cookie, int number, const char* literal)
 {
-    FEATURE_LOG_INFO("%s::%s()\n", file_tag, __FUNCTION__);
+    SubscriberHandle* sub_handle = cookie;
+    FEATURE_LOG_INFO("%s::%s() number:%d\n", file_tag, __FUNCTION__, number);
+
+    if (!sub_handle) {
+        FEATURE_LOG_ERROR("%s::%s(), sub_handle is null!", file_tag, __FUNCTION__);
+        return;
+    }
+
+    system_volume_GetRet* volumeRet = system_volumeMallocGetRet();
+    volumeRet->value = (double)(number / 10.0);
+    int cb_count = FeatureGetEventCallbackCountByName(sub_handle->feature, "onMediaValueChanged");
+    if (cb_count > 0) {
+        FeatureEmitEventByName(sub_handle->feature, "onMediaValueChanged", volumeRet);
+    }
 }
 
-void system_volume_onCreate(FeatureRuntimeContext ctx, FeatureProtoHandle handle)
-{
-    FEATURE_LOG_INFO("%s::%s()\n", file_tag, __FUNCTION__);
-}
-
-void system_volume_onRequired(FeatureRuntimeContext ctx, FeatureInstanceHandle handle)
-{
-    FEATURE_LOG_INFO("%s::%s()\n", file_tag, __FUNCTION__);
-}
-
-void system_volume_onDetached(FeatureRuntimeContext ctx, FeatureInstanceHandle handle)
-{
-    FEATURE_LOG_INFO("%s::%s()\n", file_tag, __FUNCTION__);
-}
-
-void system_volume_onDestroy(FeatureRuntimeContext ctx, FeatureProtoHandle handle)
-{
-    FEATURE_LOG_INFO("%s::%s()\n", file_tag, __FUNCTION__);
-}
-
-void system_volume_onUnregister(const char* feature_name)
-{
-    FEATURE_LOG_INFO("%s::%s()\n", file_tag, __FUNCTION__);
-}
-
-VolumeHandle* volume_malloc(FeatureInstanceHandle feature)
+static VolumeHandle* volume_malloc(FeatureInstanceHandle feature)
 {
     VolumeHandle* handle = (VolumeHandle*)malloc(sizeof(VolumeHandle));
     handle->feature = feature;
@@ -79,7 +73,7 @@ VolumeHandle* volume_malloc(FeatureInstanceHandle feature)
     return handle;
 }
 
-void volume_free(VolumeHandle* handle)
+static void volume_free(VolumeHandle* handle)
 {
     if (handle == NULL)
         return;
@@ -91,18 +85,20 @@ void volume_free(VolumeHandle* handle)
 static void finish_callback(int status, FeatureInstanceHandle feature, const char* msg,
     system_volume_GetRet* volumeRet, VolumeHandle* handle)
 {
-    if ((status == 0) && (FeatureCheckCallbackId(handle->feature, handle->success))) {
-        FeatureInvokeCallback(handle->feature, handle->success, volumeRet);
-        FeatureRemoveCallback(feature, handle->success);
-    } else if (FeatureCheckCallbackId(feature, handle->fail)) {
-        FeatureInvokeCallback(feature, handle->fail, msg, status);
-        FeatureRemoveCallback(feature, handle->fail);
-    }
+    if (handle == NULL)
+        return;
 
-    if (FeatureCheckCallbackId(handle->feature, handle->complete)) {
-        FeatureInvokeCallback(handle->feature, handle->complete, (status == 0) ? "success" : "fail");
-        FeatureRemoveCallback(handle->feature, handle->complete);
-    }
+    if ((status == 0) && (FeatureCheckCallbackId(feature, handle->success)))
+        FeatureInvokeCallback(feature, handle->success, volumeRet);
+    else if (FeatureCheckCallbackId(feature, handle->fail))
+        FeatureInvokeCallback(feature, handle->fail, msg, status);
+
+    if (FeatureCheckCallbackId(feature, handle->complete))
+        FeatureInvokeCallback(feature, handle->complete, (status == 0) ? "success" : "fail");
+
+    FeatureRemoveCallback(feature, handle->success);
+    FeatureRemoveCallback(feature, handle->fail);
+    FeatureRemoveCallback(feature, handle->complete);
     volume_free(handle);
 }
 
@@ -112,7 +108,7 @@ static void volume_set_cb(void* arg, int ret)
         return;
 
     VolumeHandle* handle = (VolumeHandle*)(arg);
-    FEATURE_LOG_INFO("[volume_set_cb:%d] ret=%d", handle->op, ret);
+    FEATURE_LOG_INFO("%s::%s() op:%d ret:%d\n", file_tag, __FUNCTION__, handle->op, ret);
     if (ret >= 0) {
         system_volume_GetRet* volumeRet = system_volumeMallocGetRet();
         volumeRet->value = handle->value;
@@ -127,7 +123,7 @@ static void volume_get_cb(void* arg, int ret, int value)
         return;
 
     VolumeHandle* handle = (VolumeHandle*)(arg);
-    FEATURE_LOG_INFO("[volume_get_cb:%d] value=%d, ret=%d", handle->op, value, ret);
+    FEATURE_LOG_INFO("%s::%s() op:%d ret:%d\n", file_tag, __FUNCTION__, handle->op, ret);
     if (ret >= 0) {
         if ((value >= 0) && (value <= 10)) {
             system_volume_GetRet* volumeRet = system_volumeMallocGetRet();
@@ -139,12 +135,68 @@ static void volume_get_cb(void* arg, int ret, int value)
         finish_callback(-1, handle->feature, "volume_get_cb volume invalid", NULL, handle);
 }
 
+void system_volume_onRegister(const char* feature_name)
+{
+    FEATURE_LOG_INFO("%s::%s()\n", file_tag, __FUNCTION__);
+}
+
+void system_volume_onCreate(FeatureRuntimeContext ctx, FeatureProtoHandle handle)
+{
+    FEATURE_LOG_INFO("%s::%s()\n", file_tag, __FUNCTION__);
+}
+
+void system_volume_onRequired(FeatureRuntimeContext ctx, FeatureInstanceHandle handle)
+{
+    FEATURE_LOG_INFO("%s::%s()\n", file_tag, __FUNCTION__);
+
+    SubscriberHandle* sub_handle = (SubscriberHandle*)malloc(sizeof(SubscriberHandle));
+    if (!sub_handle) {
+        FEATURE_LOG_ERROR("%s::%s(), calloc SubscriberHandle failed\n", file_tag, __FUNCTION__);
+        return;
+    }
+
+    sub_handle->feature = handle;
+    sub_handle->policy_handle = media_policy_subscribe(MEDIA_SCENARIO_MUSIC MEDIA_POLICY_VOLUME, volume_change_callback, sub_handle);
+    if (!sub_handle->policy_handle) {
+        FEATURE_LOG_ERROR("%s::%s(), volume subscribe failed\n", file_tag, __FUNCTION__);
+        free(sub_handle);
+        return;
+    }
+
+    FeatureSetObjectData(handle, sub_handle);
+}
+
+void system_volume_onDetached(FeatureRuntimeContext ctx, FeatureInstanceHandle handle)
+{
+    FEATURE_LOG_INFO("%s::%s()\n", file_tag, __FUNCTION__);
+    SubscriberHandle* sub_handle;
+    sub_handle = (SubscriberHandle*)FeatureGetObjectData(handle);
+    if (!sub_handle)
+        return;
+
+    if (sub_handle->policy_handle)
+        media_policy_unsubscribe(sub_handle->policy_handle);
+
+    free(sub_handle);
+}
+
+void system_volume_onDestroy(FeatureRuntimeContext ctx, FeatureProtoHandle handle)
+{
+    FEATURE_LOG_INFO("%s::%s()\n", file_tag, __FUNCTION__);
+}
+
+void system_volume_onUnregister(const char* feature_name)
+{
+    FEATURE_LOG_INFO("%s::%s()\n", file_tag, __FUNCTION__);
+}
+
 void system_volume_wrap_setMediaValue(FeatureInstanceHandle feature, AppendData data, system_volume_SetInfo* info)
 {
-    FEATURE_LOG_DEBUG("%s::%s():value=%f.", file_tag, __FUNCTION__, info->value);
+    FEATURE_LOG_INFO("%s::%s():value=%f.", file_tag, __FUNCTION__, info->value);
+
     VolumeHandle* handle = volume_malloc(feature);
     if (handle == NULL) {
-        FEATURE_LOG_ERROR("[SET] volume handle malloc failed");
+        FEATURE_LOG_ERROR("%s::%s() VolumeHandle malloc failed\n", file_tag, __FUNCTION__);
         return finish_callback(ERROR_CODE, feature, "volume malloc fail", NULL, handle);
     }
 
@@ -155,7 +207,7 @@ void system_volume_wrap_setMediaValue(FeatureInstanceHandle feature, AppendData 
     handle->complete = info->complete;
 
     if ((handle->value < 0) || (handle->value > 1)) {
-        FEATURE_LOG_ERROR("[SET] volume input argurments invalid");
+        FEATURE_LOG_ERROR("%s::%s() volume input argurments invalid\n", file_tag, __FUNCTION__);
         return finish_callback(ERROR_CODE, feature, "param is invalid", NULL, handle);
     }
 
@@ -163,17 +215,17 @@ void system_volume_wrap_setMediaValue(FeatureInstanceHandle feature, AppendData 
     int status = media_uv_policy_set_stream_volume(FeatureGetUVLoop(manager), MEDIA_STREAM_MUSIC,
         (int)(handle->value * 10), volume_set_cb, (void*)handle);
     if (status != 0) {
-        FEATURE_LOG_ERROR("[SET] media_uv_policy_set_stream_volume failed");
+        FEATURE_LOG_ERROR("%s::%s() media_uv_policy_set_stream_volume failed, ret %d\n", file_tag, __FUNCTION__, status);
         return finish_callback(ERROR_CODE, feature, "media_uv_policy_set_stream_volume fail", NULL, handle);
     }
 }
 
 void system_volume_wrap_getMediaValue(FeatureInstanceHandle feature, AppendData data, system_volume_GetInfo* info)
 {
-    FEATURE_LOG_DEBUG("%s::%s()\n", file_tag, __FUNCTION__);
+    FEATURE_LOG_INFO("%s::%s()\n", file_tag, __FUNCTION__);
     VolumeHandle* handle = volume_malloc(feature);
     if (handle == NULL) {
-        FEATURE_LOG_ERROR("[GET] volume handle malloc failed");
+        FEATURE_LOG_ERROR("%s::%s() VolumeHandle malloc failed\n", file_tag, __FUNCTION__);
         return finish_callback(ERROR_CODE, feature, "volume malloc fail", NULL, handle);
     }
 
@@ -186,7 +238,7 @@ void system_volume_wrap_getMediaValue(FeatureInstanceHandle feature, AppendData 
     int status = media_uv_policy_get_stream_volume(FeatureGetUVLoop(manager), MEDIA_STREAM_MUSIC,
         volume_get_cb, (void*)handle);
     if (status != 0) {
-        FEATURE_LOG_ERROR("[GET] media_uv_policy_get_stream_volume failed");
+        FEATURE_LOG_ERROR("%s::%s() media_uv_policy_get_stream_volume failed, ret %d\n", file_tag, __FUNCTION__, status);
         return finish_callback(ERROR_CODE, feature, "media_uv_policy_get_stream_volume failed", NULL, handle);
     }
 }
